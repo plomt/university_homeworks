841b5f0639b44aeb3e7b4963bd05e937421bd932
Lomtev Pavel <pavellomtev2012@yandex.ru>
2022-11-28 00:13:59 +0300

    version 2

--- sem1/Parallel_computing_and_software_models/create_plots.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import matplotlit.pyplot as plt
-
-
--- sem1/Parallel_computing_and_software_models/runner.sh
+++ sem1/Parallel_computing_and_software_models/runner.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-> logger.txt
+> logger0
+> logger1
+> logger2
+> logger3
 
-for var in {5..100000}
+for var in {5..500}
 do
-	./lab1 $var >> logger.txt
+	./lab0 $var >> logger0
+	./lab1 $var >> logger1
+	./lab2 $var >> logger2
+	./lab3 $var >> logger3
 done
