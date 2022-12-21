5a4322cd15e237d433f9803301f949ebc9f0fd63
Lomtev Pavel <55400213+plomt@users.noreply.github.com>
2022-12-21 08:22:35 +0300

    Update runner.sh

--- sem1/Parallel_computing_and_software_models/Lab2/runner.sh
+++ sem1/Parallel_computing_and_software_models/Lab2/runner.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-> logger0
 > logger1
 > logger2
+> logger3
+> logger4
 
 for var in `seq 1 10000 1000000`
 do
-	./lab $var >> logger0
 	./lab1 $var >> logger1
 	./lab2 $var >> logger2
+	./lab3 $var >> logger3
+	./lab4 $var >> logger4
 done
